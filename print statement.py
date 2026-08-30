ae38bd3c575971d905d69ff3db2dbb7dbc54bb8f
Samar030825 <samar030825@gmail.com>
2026-08-30 10:08:41 +0530

    Add files via upload

---
+++ print statement.py
@@ -0,0 +1,14 @@
+#Print a statement
+print(" Welcome To The World of Programing")
+
+#Print a number
+print(4)
+
+#next line after print
+print("Hello python \n")
+
+#pass multiple arguments for printing 
+print("hello" , 5)
+
+#end arguments for printing
+print("welcome to", end = "*")
